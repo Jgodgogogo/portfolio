ab40520c4ba9693c9e8142716bb69cda8cf82777
Jun <zydota@gmail.com>
2025-07-28 17:22:44 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 构建时不跑 ESLint
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   output: 'standalone', // ✅ 添加这一行
